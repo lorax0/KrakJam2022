259660c60030a6c251874be357e2364a872e8298
Franciszek Woźniak <lolw2312@gmail.com>
2022-01-26 20:30:47 +0100

    Add movement with add new input system

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] protected InputActionAsset actionAsset;
+
+        private void OnEnable()
+        {
+            this.actionAsset.Enable();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace KrakJam2022
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] protected InputActionReference movementAction;
+        [SerializeField] protected float movementSpeed = 10f;
+
+        private void FixedUpdate()
+        {
+            Vector2 moveInput = this.movementAction.action.ReadValue<Vector2>();
+            Vector3 moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
+            this.transform.Translate(moveDirection * Time.deltaTime * this.movementSpeed);
+        }
+    }
+}
