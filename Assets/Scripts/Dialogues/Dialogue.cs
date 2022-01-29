7192e71d3335af5be42a69c958bcbce25a44ebc4
deo <karol.m.kostyra@gmail.com>
2022-01-30 00:09:17 +0100

    Dialogue system in level manager

---
+++ Assets/Scripts/Dialogues/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue", menuName = "Dialogue")]
+public class Dialogue : ScriptableObject
+{
+    [SerializeField]
+    [TextArea(3, 10)]
+    private string text;
+
+    public string Text => text;
+}
