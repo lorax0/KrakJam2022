b0effa8cc1287907bec80752d0966b19de198f83
deo <karol.m.kostyra@gmail.com>
2022-01-29 17:09:30 +0100

    Created fog and level selector scene

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public class Globals
 {
     public static string scene_MainMenu = "MainMenu";
+    public static string scene_Level_01 = "Level_01";
+    public static string scene_Karol = "Karol";
 }
