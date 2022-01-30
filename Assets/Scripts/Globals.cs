4e4752312c64b0a7f1d95167e0ffc1746c794991
deo <karol.m.kostyra@gmail.com>
2022-01-30 01:43:19 +0100

    Prepared interactive items and implemented inventory

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -8,4 +8,6 @@ public class Globals
     public static string scene_Game = "Game";
     public static string scene_Level_01 = "Level_01";
     public static string scene_Karol = "Karol";
+
+    public const int mapParts = 4; 
 }
---
+++ Assets/Scripts/Items/Items/Map.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : InteractiveItem
+{
+    protected override ItemType type => ItemType.MapPart;
+
+    public override void Interact()
+    {
+        Inventory.Instance.AddItem(this);
+        gameObject.SetActive(false);
+    }
+}
